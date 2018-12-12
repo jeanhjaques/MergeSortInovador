c81d7485ebe1176badfa49d71ae48cff59080585
jeanhjaques <jeanhjaques@github.com>
2018-12-12 00:36:19 -0300

    Adiçao do método elementar

--- MergeSortInovador.java
+++ MergeSortInovador.java
@@ -2,10 +2,15 @@ public class MergeSortInovador{
 	public static void mergeSortInovador(int p, int r, int v[]){
 		int q;
 		if(p<r-1){
-			q=(p+r)/2;
-			mergeSortInovador(p,q,v);
-			mergeSortInovador(q,r,v);
-			intercala(p,q,r,v);
+			if(p+r<=8) {
+				bubleSort(p,r,v);
+			}
+			else {
+				q=(p+r)/2;
+				mergeSortInovador(p,q,v);
+				mergeSortInovador(q,r,v);
+				intercala(p,q,r,v);
+		}
 		}
 	}
 	public static void intercala(int p, int q, int r, int v[]) {
@@ -30,6 +35,29 @@ public class MergeSortInovador{
 		}
 		for(i=p;i<r;i++) {
 			v[i] = w[i-p];
+		}
+	}
+	public static void bubleSort(int p, int r,int v[]) {
+		int aux[] = new int[p+r];
+		
+		int i, j, k;
+		i=p; j=r; k=0;
+		
+		while(i<j) {
+			aux[k] = v[i]; i++; k++;
+		}
+		
+		for(int x=aux.length-1; x>0; x--) {
+			for(int y=0; y<x; y++) {
+				if(v[y]> v[y+1]) {
+					int valorAntigo = v[y];
+					v[y] = v[y+1];
+					v[y+1] = valorAntigo;
+				}
+			}
+		}
+		for(int x=0; i<aux.length; i++) {
+			v[x] = aux[x];
 		}
 	}
 }
