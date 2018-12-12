dd21deaae0a8f65333aed3d210edf76d5d042d40
jeanhjaques <jeanhjaques@github.com>
2018-12-11 23:28:37 -0300

    Adição do método intercala

--- MergeSortInovador.java
+++ MergeSortInovador.java
@@ -8,4 +8,28 @@ public class MergeSortInovador{
 			intercala(p,q,r,v);
 		}
 	}
+	public static void intercala(int p, int q, int r, int v[]) {
+		int i, j, k, w[];
+		i=p; j=q; k=0;
+		w = new int[p+r];
+
+		while(i<q && j<r) {
+			if(v[i]<v[j]) {
+				w[k] = v[i]; i++;
+			}
+			else {
+				w[k] = v[j]; j++;
+			}
+			k++;
+		}
+		while(i<q) {
+			w[k] = v[i]; i++; k++;
+		}
+		while(j<r) {
+			w[k] = v[j]; j++; k++;
+		}
+		for(i=p;i<r;i++) {
+			v[i] = w[i-p];
+		}
+	}
 }
